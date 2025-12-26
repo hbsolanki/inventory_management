c887d9d601e943ae7ac1095422f8493fb2eb9076
Solanki Hardik <sh369915@gmail.com>
2025-12-26 11:10:56 +0530

    register password hashing problem solve

--- apps/userprofile/serializers/UserCreate.py
+++ apps/userprofile/serializers/UserCreate.py
@@ -8,3 +8,12 @@ class UserProfileCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model=UserProfile
         fields=["first_name","last_name","username","password","email","profile_picture"]
+
+    def create(self, validated_data):
+        password=validated_data.get("password")
+        user=UserProfile(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
+    
