034e99208b71e5cfdbea5328b73fa9f56c91ca3e
Solanki Hardik <sh369915@gmail.com>
2025-12-29 10:59:25 +0530

    productread add in item trans

--- apps/inventory_transaction/serializers/Inventory_transaction_read.py
+++ apps/inventory_transaction/serializers/Inventory_transaction_read.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from ..models import InventoryTransaction,InventoryTransactionItem
+from ...product.serializers.product_read import ProductReadSerializer
 
 class InventoryTransItemSerializer(serializers.ModelSerializer):
-    
+    product=ProductReadSerializer(read_only=True)
     class Meta:
         model=InventoryTransactionItem
         fields=["id","product","quantity"]
